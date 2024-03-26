2abb0224302848b33b170137b5a01c7e079d09e4
ItsChaco26 <aaronchacon2601@gmail.com>
2024-03-25 22:39:42 -0600

    Botón del carrito listo, falta la parte lógica

--- C22037/my-app/app/cart/page.tsx
+++ C22037/my-app/app/cart/page.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function CartPage() {
   return (
     <div>
-      <h1>¡Esta es la página del carrito de compras!</h1>
-      <Link href="/">Volver a la página de inicio</Link>
+      <h1>Amazon Cart</h1>
+      <Link href="/">Home</Link>
     </div>
   );
 }
