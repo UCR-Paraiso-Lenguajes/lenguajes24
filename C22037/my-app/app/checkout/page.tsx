e5bd4d3e8a64ebfea0db256474ab335bb4f83f7c
ItsChaco26 <aaronchacon2601@gmail.com>
2024-03-29 23:29:14 -0600

    Página Payment creada, correcciones de nombres y variables en checkout

--- C22037/my-app/app/checkout/page.tsx
+++ C22037/my-app/app/checkout/page.tsx
@@ -3,7 +3,24 @@ import { useEffect, useState } from 'react';
 import "@/public/styles.css";
 import Link from 'next/link';
 
-export default function CartPage() {
+export default function Address() {
+    const [address, setAddress] = useState('');
+
+    const handleAddressChange = (e) => {
+        setAddress(e.target.value);
+    };
+
+    const handleSaveAddress = () => {
+        localStorage.setItem('address', address);
+    };
+
+    useEffect(() => {
+        const storedAddress = localStorage.getItem('address');
+        if (storedAddress) {
+            setAddress(storedAddress);
+        }
+    }, []);
+
     return (
         <div>
             <div className="header">
@@ -13,14 +30,24 @@ export default function CartPage() {
             </div>
 
             <div className="body">
-                <h2>Payment Method</h2>
-                
+                <h2>Address</h2>
+                <form>
+                    <input
+                        type="text"
+                        id="address"
+                        name="address"
+                        value={address}
+                        onChange={handleAddressChange}/>
+                </form>
+                <Link href="/payment">
+                    <button className="Button" onClick={handleSaveAddress}>Save Address</button>
+                </Link>
+                    
             </div>
 
             <div className="footer">
                 <h2>Amazon.com</h2>
             </div>
-
         </div>
     );
 }
