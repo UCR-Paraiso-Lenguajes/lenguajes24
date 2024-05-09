0fb48b068ea42239158aaf2a2a4aa3dc6d58af55
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-05-09 17:15:00 -0600

    Actualizar Categories.cs

--- C12599/lab10/Api/Core/models/Categories.cs
+++ C12599/lab10/Api/Core/models/Categories.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace storeapi.Models
 {
-    public class Category
+    public struct Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
