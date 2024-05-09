4f09a0c9578d63ca1433b59f9981ecdfe1fb2870
Sofia Cordero <marianoda20027@gmail.com>
2024-05-09 03:21:32 -0600

    Tarea3

--- C12599/lab10/Api/storeapi/Controllers/StoreController.cs
+++ C12599/lab10/Api/storeapi/Controllers/StoreController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using storeapi.Models;
+using storeapi.Models;
 using System;
+using System.Collections.Generic;
 
 namespace storeapi.Controllers
 {
@@ -17,7 +19,24 @@ namespace storeapi.Controllers
             {
                 throw new InvalidOperationException("No se pudo obtener la instancia de la tienda.");
             }
-            return Ok(storeInstance);
+
+            Categories categories = new Categories();
+
+            // Crear una lista de objetos para almacenar las categorías
+            List<object> categoryList = new List<object>();
+            
+            foreach (var category in categories.ListCategories)
+            {
+                categoryList.Add(category);
+            }
+
+            var response = new Dictionary<string, IEnumerable<object>>
+            {
+                ["Products"] = storeInstance.Products,
+                ["Categories"] = categoryList  
+            };
+
+            return Ok(response);
         }
     }
 }
