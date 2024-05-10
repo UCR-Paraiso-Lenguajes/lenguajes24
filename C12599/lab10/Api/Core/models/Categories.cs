a31d7f3d20e7e4ee5e0e8b77b7880e03a54370b6
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-05-10 14:34:57 -0600

    Actualizar Categories.cs

--- C12599/lab10/Api/Core/models/Categories.cs
+++ C12599/lab10/Api/Core/models/Categories.cs
@@ -3,56 +3,36 @@ using System.Collections.Generic;
 
 namespace storeapi.Models
 {
-    public  struct Category
+    public struct Category
     {
-        private int Id { get; set; }
-        private string Name { get; set; }
+        private int Id;
+        private string Name;
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 
     public class Categories
     {
         public List<Category> ListCategories { get; } = new List<Category>
         {
-            new Category { Id = 1, Name = "Electrónica" },
-            new Category { Id = 2, Name = "Moda" },
-            new Category { Id = 3, Name = "Hogar y jardín" },
-            new Category { Id = 4, Name = "Deportes y actividades al aire libre" },
-            new Category { Id = 5, Name = "Belleza y cuidado personal" },
-            new Category { Id = 6, Name = "Alimentación y bebidas" },
-            new Category { Id = 7, Name = "Libros y entretenimiento" },
-            new Category { Id = 8, Name = "Tecnología" },
-            new Category { Id = 9, Name = "Deportes" }
+            new Category(1, "Electrónica"),
+            new Category(2, "Moda"),
+            new Category(3, "Hogar y jardín"),
+            new Category(4, "Deportes y actividades al aire libre"),
+            new Category(5, "Belleza y cuidado personal"),
+            new Category(6, "Alimentación y bebidas"),
+            new Category(7, "Libros y entretenimiento"),
+            new Category(8, "Tecnología"),
+            new Category(9, "Deportes")
         };
 
         public Categories()
         {
-        
             ListCategories.Sort((x, y) => string.Compare(x.Name, y.Name, StringComparison.OrdinalIgnoreCase));
         }
-
-  public int GetCategoryId(string categoryId)
-{
-    // Validar que el categoryId no sea nulo o vacío
-    if (string.IsNullOrWhiteSpace(categoryId))
-    {
-        throw new ArgumentNullException(nameof(categoryId), "El Id de la categoría no puede ser nulo o vacío.");
     }
-
-   
-    if (int.TryParse(categoryId, out int parsedCategoryId))
-    {
-  
-        foreach (Category category in ListCategories)
-        {
-            if (category.Id == parsedCategoryId)
-            {
-                return parsedCategoryId; // Devuelve el Id de la categoría encontrada
-            }
-        }
-    }
-
-    // Si no se puede convertir a entero o no se encuentra la categoría, lanzar una excepción
-    throw new ArgumentException($"La categoría con Id '{categoryId}' no fue encontrada o el formato de Id no es válido.");
-}
-}
 }
