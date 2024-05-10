7f5a0d0fc3a38db840a84f4c0293215d92f7be23
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-05-09 21:35:33 -0600

    Update ProductsController.cs

--- C12599/lab10/Api/storeapi/Controllers/ProductsController.cs
+++ C12599/lab10/Api/storeapi/Controllers/ProductsController.cs
@@ -3,25 +3,28 @@ using System.Linq;
 using storeapi.Database;
 using storeapi.Models;
 using storeapi.Models;
+
 namespace storeapi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
-    
+
         [HttpGet("GetProducts")]
-        public IActionResult GetProducts([FromQuery] string category)
+        public IActionResult GetProducts([FromQuery] string categoryID)
         {
-            if (string.IsNullOrWhiteSpace(category))
+
+            if (string.IsNullOrWhiteSpace(categoryID))
             {
                 return BadRequest("La categoría no puede estar vacía.");
-            }    
+            }
+
             Categories categories = new Categories();
-            string idCategory = categories.GetCategoryIdByName(category);
+            int idCategoryparsed = categories.GetCategoryId(categoryID);
 
             Products products = new Products();
-            return Ok(products.LoadProductsFromDatabase(idCategory));
+            return Ok(products.LoadProductsFromDatabase(idCategoryparsed));
         }
     }
 }
