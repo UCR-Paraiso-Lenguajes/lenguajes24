aa9dec88fe9c52bf6a0c648e78d294d173c93a7a
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-05-10 21:49:08 -0600

    Update page.tsx

--- C12599/lab10/laboratorio6/laboratorio6/app/page.tsx
+++ C12599/lab10/laboratorio6/laboratorio6/app/page.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-
 const Page = () => {
   const [state, setState] = useState({
     cart: {
@@ -24,14 +23,13 @@ const Page = () => {
         throw new Error('Failed to fetch data');
       }
       const json = await response.json();
-      const productList = json.Products || [];
-      const categories = json.Categories || [];
+      const { products, categories } = json;
 
-      setState(prevState => ({
-        ...prevState,
-        productList,
-        categories,
-      }));
+      setState({
+        ...state,
+        productList: products || [],
+        categories: categories || [],
+      });
     };
 
     fetchData();
@@ -41,27 +39,27 @@ const Page = () => {
     if (!product || typeof product !== 'object' || !product.hasOwnProperty('id') || !product.hasOwnProperty('name') || !product.hasOwnProperty('price')) {
       throw new Error('Invalid product object');
     }
+
     const { cart } = state;
     const { productos, count } = cart;
     const isProductInCart = productos.some(item => item.id === product.id);
-    if (isProductInCart) {
+
+    if (!isProductInCart) {
+      const updatedCart = {
+        ...cart,
+        productos: [...productos, product],
+        count: count + 1,
+      };
+
+      setState({
+        ...state,
+        cart: updatedCart,
+      });
+
+      localStorage.setItem('cartData', JSON.stringify(updatedCart));
+    } else {
       throw new Error('Product is already in the cart');
     }
-    if (typeof product.price !== 'number' || product.price <= 0) {
-      throw new Error('Product price must be a positive number');
-    }
-    const updatedCart = {
-      ...cart,
-      productos: [...productos, product],
-      count: count + 1,
-    };
-
-    setState(prevState => ({
-      ...prevState,
-      cart: updatedCart,
-    }));
-
-    localStorage.setItem('cartData', JSON.stringify(updatedCart));
   };
 
   const handleCategoryChange = async (event) => {
@@ -69,7 +67,7 @@ const Page = () => {
       throw new Error('Invalid event object');
     }
 
-    const selectedCategoryId = event.target.value.trim(); // Obtener el valor y eliminar espacios en blanco
+    const selectedCategoryId = event.target.value;
 
     if (selectedCategoryId === '') {
       const response = await fetch('https://localhost:7043/api/Store');
@@ -77,44 +75,35 @@ const Page = () => {
         throw new Error('Failed to fetch products');
       }
       const json = await response.json();
-      const productList = json.Products || [];
+      const { products } = json;
 
-      setState(prevState => ({
-        ...prevState,
-        productList,
+      setState({
+        ...state,
+        productList: products || [],
         selectedCategory: '',
-      }));
+      });
+    } else {
+     
+      const response = await fetch(`https://localhost:7043/api/Products?categoryId=${selectedCategoryId}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch products');
+      }
+      const json = await response.json();
+      const productList = json || [];
 
-      return;
+      setState({
+        ...state,
+        productList,
+        selectedCategory: selectedCategoryId,
+      });
     }
-
-    const categoryId = selectedCategoryId.replace(/\D/g, '');
-
-    if (!categoryId) {
-      throw new Error('Invalid category ID');
-    }
-
-    const response = await fetch(`https://localhost:7043/api/Products?categoryId=${categoryId}`);
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch products');
-    }
-    const json = await response.json();
-    const productList = json || [];
-
-    setState(prevState => ({
-      ...prevState,
-      productList,
-      selectedCategory: categoryId,
-    }));
-
   };
 
-  const renderDropdown = () => {
-    const { categories, selectedCategory } = state;
+  const renderCategories = () => {
+    const { categories } = state;
 
     return (
-      <select value={selectedCategory} onChange={handleCategoryChange} className="form-select mb-3">
+      <select value={state.selectedCategory} onChange={handleCategoryChange} className="form-select mb-3">
         <option value="">Todas las categorías</option>
         {categories.map(category => (
           <option key={category.id} value={category.id}>
@@ -125,58 +114,63 @@ const Page = () => {
     );
   };
 
-  const renderCarouselItems = () => {
+  const renderProducts = () => {
     const { productList } = state;
 
-    if (!productList || productList.length === 0) {
-      return [];
-    }
-
-    return productList.map((product) => (
-      <Carousel.Item key={product.id}>
-        <div className="text-center">
-          <img src={product.imageUrl} alt={product.name} style={{ maxHeight: '300px' }} />
-          <h3>{product.name}</h3>
-          <p>{product.description}</p>
-          <p>Precio: ${product.price}</p>
-          <button className="btn btn-primary" onClick={() => handleAddToCart(product)}>
-            Comprar
-          </button>
+    return productList.map(product => (
+      <div key={product.id} className="col-sm-3 mb-4">
+        <div className="card">
+          <img src={product.imageUrl} className="card-img-top" alt={product.name} />
+          <div className="card-body">
+            <h5 className="card-title">{product.name}</h5>
+            <p className="card-text">{product.description}</p>
+            <p className="card-text">Precio: ${product.price}</p>
+            <p className="card-text"> Categoria: {product.category.name} </p>
+            <button className="btn btn-primary" onClick={() => handleAddToCart(product)}>
+              Comprar
+            </button>
+          </div>
         </div>
-      </Carousel.Item>
+      </div>
     ));
   };
 
-  const renderGridItems = () => {
-    const { productList, categories } = state;
-
-    if (!productList || productList.length === 0 || !categories || categories.length === 0) {
-      return [];
+  
+  const renderCarouselItems = () => {
+  
+  
+    if (!state.productList || state.productList.length === 0) {
+      return null; 
     }
-
-    return productList.map((product) => {
-      const category = categories.find((cat) => cat.id === product.categoryID);
-
-      return (
-        <div key={product.id} className="col-sm-3 mb-4">
-          <div className="card custom-card"> {/* Agrega una clase para estilos personalizados */}
-            <img src={product.imageUrl} className="card-img-top" alt={product.name} />
-            <div className="card-body">
-              <div className="text-center">
-                <h5 className="card-title my-3">{product.name}</h5>
-                <p className="card-text my-3">{product.description}</p>
-                <p className="card-text my-3">Precio: ${product.price}</p>
-                {category && <p className="card-text my-3">Categoría: {category.name}</p>}
+  
+    const featuredProducts = state.productList.slice(0, 5); 
+  
+    return (
+      <Carousel>
+        {featuredProducts.map((product) => (
+          <Carousel.Item key={product.id}>
+            <div className="d-flex flex-column align-items-center">
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                style={{ maxHeight: '300px', maxWidth: '100%', objectFit: 'contain' }}
+              />
+              <div className="mt-3 text-center">
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <p>Precio: ${product.price}</p>
+                <p>Categoria: {product.category.name} </p>
                 <button className="btn btn-primary" onClick={() => handleAddToCart(product)}>
                   Comprar
                 </button>
               </div>
             </div>
-          </div>
-        </div>
-      );
-    });
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    );
   };
+  
   return (
     <div className="container">
       <div className="row">
@@ -189,29 +183,17 @@ const Page = () => {
           </form>
         </div>
         <div className="col-sm-2">
-          <div className="my-3" style={{ position: 'relative', display: 'inline-block' }}>
+          <div className="my-3">
             <Link href="/cart">
               <button className="btn btn-primary">
                 <i className="bi bi-cart-fill"></i>
               </button>
             </Link>
-            <span
-              className="badge rounded-pill bg-danger"
-              style={{
-                position: 'absolute',
-                top: '-10px',
-                right: '-10px',
-                fontSize: '0.8rem',
-                minWidth: '20px',
-                padding: '5px',
-              }}
-            >
-              {state.cart.count}
-            </span>
+            <span className="badge rounded-pill bg-danger">{state.cart.count}</span>
           </div>
         </div>
         <div className="col-sm-2">
-          <div className="my-3" style={{ position: 'relative', display: 'inline-block' }}>
+          <div className="my-3">
             <Link href="/admin">
               <button className="btn btn-primary">
                 <i className="bi bi-person"></i>
@@ -220,18 +202,20 @@ const Page = () => {
           </div>
         </div>
       </div>
-      <div className='row'>
-        {renderDropdown()}
+      <div className="row">
+        <div className="col-sm-4">
+          {renderCategories()}
+        </div>
       </div>
       <div className="row">
         <h1 className="mb-0">Lista de Productos</h1>
       </div>
       <div className="row row-cols-4 g-4">
-        {renderGridItems()}
+        {renderProducts()}
       </div>
       <div className="row mt-4">
         <h2 className="mb-3">Productos Destacados</h2>
-        <Carousel>{renderCarouselItems()}</Carousel>
+        {renderCarouselItems()}
       </div>
       <footer className="footer mt-auto py-3" style={{ backgroundColor: '#ADD8E6' }}>
         <div className="container">
