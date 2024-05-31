c420bdc3645520b2cd7c4b4369143933e8849c2e
EisenheimDiego <corey1898@hotmail.com>
2024-05-30 23:39:28 -0600

    Cambios solicitados

--- B66958/Labs/Lab7/UT/AuthTests.cs
+++ B66958/Labs/Lab7/UT/AuthTests.cs
@@ -1,9 +1,9 @@
 using ApiLab7;
 using ApiLab7.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace UT;
@@ -19,22 +19,28 @@ public class AuthTests
         credentialOptionsMock = new Mock<IOptions<CredentialOptions>>();
         hostEnvironmentMock = new Mock<IHostEnvironment>();
         hostEnvironmentMock.Setup(h => h.EnvironmentName).Returns("Development");
-        credentialOptionsMock.Setup(x => x.Value).Returns(new CredentialOptions
-        {
-            Development = new List<UserAuth>
-            {
-                new UserAuth { Name = "Diego", Password = "secret", Role = "Admin" },
-                new UserAuth { Name = "Eisenheim", Password = "secret", Role = "Operator" }
-            }
-        });
+        credentialOptionsMock
+            .Setup(x => x.Value)
+            .Returns(
+                new CredentialOptions(
+                    new List<UserAuth>
+                    {
+                        new UserAuth("Diego", "secret", "Admin"),
+                        new UserAuth("Eisenheim", "secret", "Operator")
+                    }
+                )
+            );
     }
 
     [Test]
     public async Task SucessfullAuthenticationReturnsOK()
     {
-        var user = new UserAuth{Name="Diego", Password="secret"};
+        var user = new UserAuth("Diego", "secret", null);
 
-        AuthController authController = new AuthController(hostEnvironmentMock.Object, credentialOptionsMock.Object);
+        AuthController authController = new AuthController(
+            hostEnvironmentMock.Object,
+            credentialOptionsMock.Object
+        );
         var result = await authController.LoginAsync(user);
         Assert.IsNotNull(result);
         Assert.IsInstanceOf<OkObjectResult>(result);
@@ -43,9 +49,12 @@ public class AuthTests
     [Test]
     public async Task ForbiddenAuthenticationReturnsUnathorized()
     {
-        var user = new UserAuth{Name="Diego", Password="incorrecta"};
+        var user = new UserAuth("Diego", "incorrecta", null);
 
-        AuthController authController = new AuthController(hostEnvironmentMock.Object, credentialOptionsMock.Object);
+        AuthController authController = new AuthController(
+            hostEnvironmentMock.Object,
+            credentialOptionsMock.Object
+        );
         var result = await authController.LoginAsync(user);
         Assert.IsNotNull(result);
         Assert.IsInstanceOf<UnauthorizedResult>(result);
