914c98d046c58a4d65f62209da7e612ecbbd8e30
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-06-19 23:15:29 -0600

    Update Products.cs

--- C12599/Examen1/Api/UT/Products.cs
+++ C12599/Examen1/Api/UT/Products.cs
@@ -7,8 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 using core;
-
 using storeapi.Database;
+
 namespace UT
 {
     [TestFixture]
@@ -24,8 +24,8 @@ namespace UT
 
             var dbtestDefault = "Server=localhost;Database=lab;Uid=root;Pwd=123456;";
             DataConnection.Init(dbtestDefault);
-                        DatabaseInitializer.Initialize();
-
+            DatabaseInitializer.Initialize();
+            _insertProductsLogic = new InsertProductsLogic(_memoryCache, StoreDB.InsertProduct);
         }
 
         [TearDown]
@@ -64,11 +64,11 @@ namespace UT
             var existingProduct = new Product
             {
                 id = 1,
-                Name = "Existing Product",
-                Price = 19,
+                Name = "Updated Product",
+                Price = 29,
                 ImageUrl = "http://example.com/image.png",
-                Description = "Existing Product Description",
-                Category = new Category { _id = 1, _name = "Existing Category" }
+                Description = "Updated Product Description",
+                Category = new Category { _id = 1, _name = "Updated Category" }
             };
             _memoryCache.Set("Products", new List<Product> { existingProduct });
 
@@ -92,7 +92,7 @@ namespace UT
             Assert.AreEqual(updatedProduct.Price, productInCache.Price);
             Assert.AreEqual(updatedProduct.ImageUrl, productInCache.ImageUrl);
             Assert.AreEqual(updatedProduct.Description, productInCache.Description);
-            Assert.AreEqual(updatedProduct.Category.Name, productInCache.Category.Name);
+            Assert.AreEqual(updatedProduct.Category._name, productInCache.Category._name);
         }
 
         private void InsertProductToList(Product product, List<Product> products)
@@ -118,3 +118,4 @@ namespace UT
         }
     }
 }
+
