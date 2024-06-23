8e2d2d993d8eede1796fb9c3aa634013f18e70aa
ItsChaco26 <aaronchacon2601@gmail.com>
2024-06-22 18:16:25 -0600

    Terminado

--- C22037/Lab12/my-app/app/page.tsx
+++ C22037/Lab12/my-app/app/page.tsx
@@ -84,19 +84,24 @@ export default function Home() {
 
     loadData();
 
-    // Load notifications
     const storedNotifications = JSON.parse(localStorage.getItem('notifications') || '[]');
     setNotifications(storedNotifications);
 
-    // Setup SignalR connection
     const connection = new HubConnectionBuilder()
       .withUrl("http://localhost:5087/chatHub")
       .withAutomaticReconnect()
       .build();
 
     connection.start()
-      .then(() => console.log("Connection to SignalR hub established"))
+      .then(() => {
+        console.log("Connection to SignalR hub established");
+        connection.invoke("SendRecentMessages");
+      })
       .catch(err => console.error('Error connecting to SignalR hub:', err));
+
+    connection.on('ReceiveRecentMessages', (messages) => {
+      setNotifications(messages.map(msg => ({ id: msg.id, message: msg.content, read: false })));
+    });
 
     connection.on('ReceiveAllMessages', (messages) => {
       setNotifications(messages.map(msg => ({ id: msg.id, message: msg.content, read: false })));
@@ -328,7 +333,7 @@ export default function Home() {
               <Dropdown>
                 <Dropdown.Toggle as="a" bsPrefix="notification-toggle">
                   <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-bell" viewBox="0 0 16 16">
-                    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+                    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6" />
                   </svg>
                   {notifications.filter(notification => !notification.read).length > 0 && (
                     <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
