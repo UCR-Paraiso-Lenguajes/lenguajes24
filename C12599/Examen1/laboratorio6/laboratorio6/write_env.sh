e8feaf78c8834177c64504a46cd7839f27b9e447
Cristian Guillen Mendez <cristianguillenmendez@Cristians-MacBook-Pro.local>
2024-06-28 11:29:57 -0600

    cleaning ols code

--- C12599/Examen1/laboratorio6/laboratorio6/write_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "$NEXT_PUBLIC_API" ]; then
-  echo "Error: NEXT_PUBLIC_API is not set."
-fi
-
-# Escribe la variable de entorno en el archivo .env
-echo "NEXT_PUBLIC_API=$NEXT_PUBLIC_API" > .env
-<
