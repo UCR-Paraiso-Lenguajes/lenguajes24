e6dd5083b91d3f02c113c588b0b01b1859d578ee
Kendall Sánchez <117062888+Kensc19@users.noreply.github.com>
2024-06-28 22:08:20 -0600

    Arreglos en el código

--- C27227/Examen1_C27227/Core/Data/DatabaseStore.cs
+++ C27227/Examen1_C27227/Core/Data/DatabaseStore.cs
@@ -153,13 +153,12 @@ namespace KEStoreApi.Data
         Id = 15,
         Name = "GIGABYTE GeForce RTX 4060 AERO OC 8G Graphics Card",
         Price = 319,
-        ImageUrl = "https://static.tweaktown.com/news/9/0/90778_02_gigabyte-accidentally-confirms-geforce-rtx-4070-12gb-and-4060-8gb_full.jpg",
+        ImageUrl = "https://static.gigabyte.com/StaticFile/Image/Global/dc8aeef6fa8e07dd2202a33fb0c59266/Product/35407/Png",
         Quantity = 1,
         CategoriaId = Categorias.Instance.GetCategorias().ToList().First(c => c.Nombre == "Hardware").Id,
         Description = "Tarjeta gráfica GIGABYTE GeForce RTX 4060, ideal para gaming en alta resolución."
     }
 };
-
 
             string connectionString = DatabaseConfiguration.Instance.ConnectionString;
 
@@ -172,80 +171,78 @@ namespace KEStoreApi.Data
                     try
                     {
                         string createTableQuery = @"
-                        DROP DATABASE IF EXISTS store;
-                        CREATE DATABASE store;
-                        USE store;
-                        CREATE TABLE IF NOT EXISTS products (
-                            id INT AUTO_INCREMENT PRIMARY KEY,
-                            name VARCHAR(100) NOT NULL,
-                            price DECIMAL(10, 2) NOT NULL,
-                            ImageUrl VARCHAR(500) NOT NULL,
-                            idCategoria INT NOT NULL,
-                            description VARCHAR(500) NOT NULL,
-                            isDeleted BOOLEAN DEFAULT FALSE
-                        );
-                        CREATE TABLE IF NOT EXISTS paymentMethod (
-                            id INT PRIMARY KEY,
-                            description VARCHAR(50) NOT NULL
-                        );
-                        INSERT INTO paymentMethod (id, description) VALUES (0, 'Cash');
-                        INSERT INTO paymentMethod (id, description) VALUES (1, 'Sinpe');
+                            DROP DATABASE IF EXISTS store;
+                            CREATE DATABASE store;
+                            USE store;
+                            CREATE TABLE IF NOT EXISTS products (
+                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                name VARCHAR(100) NOT NULL,
+                                price DECIMAL(10, 2) NOT NULL,
+                                ImageUrl VARCHAR(500) NOT NULL,
+                                idCategoria INT NOT NULL,
+                                description VARCHAR(500) NOT NULL,
+                                isDeleted BOOLEAN DEFAULT FALSE
+                            );
+                            CREATE TABLE IF NOT EXISTS paymentMethod (
+                                id INT PRIMARY KEY,
+                                description VARCHAR(50) NOT NULL
+                            );
+                            INSERT INTO paymentMethod (id, description) VALUES (0, 'Cash');
+                            INSERT INTO paymentMethod (id, description) VALUES (1, 'Sinpe');
 
-                        CREATE TABLE IF NOT EXISTS Sales (
-                            purchaseNumber VARCHAR(50) NOT NULL PRIMARY KEY,
-                            total DECIMAL(10, 2) NOT NULL,
-                            purchase_date DATETIME NOT NULL,
-                            payment_method INT NOT NULL,
-                            FOREIGN KEY (payment_method) REFERENCES paymentMethod(id)
-                        );
+                            CREATE TABLE IF NOT EXISTS Sales (
+                                purchaseNumber VARCHAR(50) NOT NULL PRIMARY KEY,
+                                total DECIMAL(10, 2) NOT NULL,
+                                purchase_date DATETIME NOT NULL,
+                                payment_method INT NOT NULL,
+                                FOREIGN KEY (payment_method) REFERENCES paymentMethod(id)
+                            );
 
-                        CREATE TABLE IF NOT EXISTS Lines_Sales (
-                            id_line INT AUTO_INCREMENT PRIMARY KEY,
-                            id_Sale VARCHAR(50) NOT NULL,
-                            id_Product INT NOT NULL,
-                            quantity INT NOT NULL,
-                            price DECIMAL(10, 2) NOT NULL,
-                            FOREIGN KEY (id_Sale) REFERENCES Sales(purchaseNumber),
-                            FOREIGN KEY (id_Product) REFERENCES products(id)
-                        );
+                            CREATE TABLE IF NOT EXISTS Lines_Sales (
+                                id_line INT AUTO_INCREMENT PRIMARY KEY,
+                                id_Sale VARCHAR(50) NOT NULL,
+                                id_Product INT NOT NULL,
+                                quantity INT NOT NULL,
+                                price DECIMAL(10, 2) NOT NULL,
+                                FOREIGN KEY (id_Sale) REFERENCES Sales(purchaseNumber),
+                                FOREIGN KEY (id_Product) REFERENCES products(id)
+                            );
 
-                        -- Semana del 7 de abril al 13 de abril
-                        INSERT INTO Sales (purchase_date, total, payment_method, purchaseNumber)
-                        VALUES 
-                            ('2024-04-07 10:00:00', 280.00, 0, 'PUR001'),
-                            ('2024-04-08 10:00:00', 480.00, 1, 'PUR002'),
-                            ('2024-04-09 12:00:00', 910.00, 1, 'PUR003'),
-                            ('2024-04-10 14:00:00', 450.00, 0, 'PUR004'),
-                            ('2024-04-11 16:00:00', 340.00, 1, 'PUR005'),
-                            ('2024-04-12 18:00:00', 650.00, 0, 'PUR006'),
-                            ('2024-04-13 20:00:00', 530.00, 1, 'PUR007');
+                            -- Semana del 7 de abril al 13 de abril
+                            INSERT INTO Sales (purchase_date, total, payment_method, purchaseNumber)
+                            VALUES 
+                                ('2024-04-07 10:00:00', 280.00, 0, 'PUR001'),
+                                ('2024-04-08 10:00:00', 480.00, 1, 'PUR002'),
+                                ('2024-04-09 12:00:00', 910.00, 1, 'PUR003'),
+                                ('2024-04-10 14:00:00', 450.00, 0, 'PUR004'),
+                                ('2024-04-11 16:00:00', 340.00, 1, 'PUR005'),
+                                ('2024-04-12 18:00:00', 650.00, 0, 'PUR006'),
+                                ('2024-04-13 20:00:00', 530.00, 1, 'PUR007');
 
-                        -- Semana del 14 de abril al 20 de abril
-                        INSERT INTO Sales (purchase_date, total, payment_method, purchaseNumber)
-                        VALUES 
-                            ('2024-04-14 10:00:00', 390.00, 0, 'PUR008'),
-                            ('2024-04-15 12:00:00', 520.00, 1, 'PUR009'),
-                            ('2024-04-16 14:00:00', 930.00, 0, 'PUR010'),
-                            ('2024-04-17 16:00:00', 950.00, 1, 'PUR011'),
-                            ('2024-04-18 18:00:00', 550.00, 0, 'PUR012'),
-                            ('2024-04-19 20:00:00', 460.00, 1, 'PUR013'),
-                            ('2024-04-20 22:00:00', 620.00, 0, 'PUR014');
-                    ";
+                            -- Semana del 14 de abril al 20 de abril
+                            INSERT INTO Sales (purchase_date, total, payment_method, purchaseNumber)
+                            VALUES 
+                                ('2024-04-14 10:00:00', 390.00, 0, 'PUR008'),
+                                ('2024-04-15 12:00:00', 520.00, 1, 'PUR009'),
+                                ('2024-04-16 14:00:00', 930.00, 0, 'PUR010'),
+                                ('2024-04-17 16:00:00', 950.00, 1, 'PUR011'),
+                                ('2024-04-18 18:00:00', 550.00, 0, 'PUR012'),
+                                ('2024-04-19 20:00:00', 460.00, 1, 'PUR013'),
+                                ('2024-04-20 22:00:00', 620.00, 0, 'PUR014');
+                        ";
 
                         using (var command = new MySqlCommand(createTableQuery, connection, transaction))
                         {
                             int result = command.ExecuteNonQuery();
                             if (result < 0)
-                            {
-                                throw new Exception("Error creating the database");
-                            }
+                                throw new Exception("Error creating database tables");
                         }
 
                         foreach (Product product in products)
                         {
                             string insertProductQuery = @"
-                            INSERT INTO products(name, price, ImageUrl, idCategoria, description)
-                            VALUES(@name, @price, @imageUrl, @categoryId, @description);";
+                                INSERT INTO products(name, price, ImageUrl, idCategoria, description)
+                                VALUES(@name, @price, @imageUrl, @categoryId, @description);";
 
                             using (var insertCommand = new MySqlCommand(insertProductQuery, connection, transaction))
                             {
@@ -259,61 +256,59 @@ namespace KEStoreApi.Data
                         }
 
                         string insertQuery = @"
-                        INSERT INTO Lines_Sales (id_Sale, id_Product, quantity, price)
-                        VALUES 
-                            ('PUR001', 1, 2, 100.00),
-                            ('PUR001', 3, 3, 20.00),
-                            ('PUR001', 4, 1, 80.00),
-                            ('PUR002', 1, 1, 100.00),
-                            ('PUR002', 2, 2, 60.00),
-                            ('PUR002', 4, 1, 80.00),
-                            ('PUR003', 2, 1, 60.00),
-                            ('PUR003', 3, 3, 60.00),
-                            ('PUR003', 4, 2, 80.00),
-                            ('PUR004', 1, 1, 100.00),
-                            ('PUR004', 3, 2, 40.00),
-                            ('PUR004', 4, 1, 80.00),
-                            ('PUR005', 2, 1, 60.00),
-                            ('PUR005', 4, 2, 80.00),
-                            ('PUR006', 1, 2, 100.00),
-                            ('PUR006', 2, 1, 60.00),
-                            ('PUR007', 3, 1, 20.00),
-                            ('PUR007', 4, 2, 80.00);
+                            INSERT INTO Lines_Sales (id_Sale, id_Product, quantity, price)
+                            VALUES 
+                                ('PUR001', 1, 2, 100.00),
+                                ('PUR001', 3, 3, 20.00),
+                                ('PUR001', 4, 1, 80.00),
+                                ('PUR002', 1, 1, 100.00),
+                                ('PUR002', 2, 2, 60.00),
+                                ('PUR002', 4, 1, 80.00),
+                                ('PUR003', 2, 1, 60.00),
+                                ('PUR003', 3, 3, 60.00),
+                                ('PUR003', 4, 2, 80.00),
+                                ('PUR004', 1, 1, 100.00),
+                                ('PUR004', 3, 2, 40.00),
+                                ('PUR004', 4, 1, 80.00),
+                                ('PUR005', 2, 1, 60.00),
+                                ('PUR005', 4, 2, 80.00),
+                                ('PUR006', 1, 2, 100.00),
+                                ('PUR006', 2, 1, 60.00),
+                                ('PUR007', 3, 1, 20.00),
+                                ('PUR007', 4, 2, 80.00);
 
-                        INSERT INTO Lines_Sales (id_Sale, id_Product, quantity, price)
-                        VALUES 
-                            ('PUR008', 1, 1, 100.00),
-                            ('PUR008', 3, 3, 60.00),
-                            ('PUR008', 4, 1, 80.00),
-                            ('PUR009', 2, 1, 60.00),
-                            ('PUR009', 4, 2, 80.00),
-                            ('PUR010', 1, 2, 100.00),
-                            ('PUR010', 2, 1, 60.00),
-                            ('PUR010', 3, 2, 40.00),
-                            ('PUR011', 1, 2, 100.00),
-                            ('PUR011', 3, 1, 20.00),
-                            ('PUR011', 4, 2, 80.00),
-                            ('PUR012', 2, 1, 60.00),
-                            ('PUR012', 4, 2, 80.00),
-                            ('PUR013', 1, 1, 100.00),
-                            ('PUR013', 3, 3, 60.00),
-                            ('PUR014', 2, 1, 60.00),
-                            ('PUR014', 3, 1, 20.00),
-                            ('PUR014', 4, 2, 80.00);
-                    ";
+                            INSERT INTO Lines_Sales (id_Sale, id_Product, quantity, price)
+                            VALUES 
+                                ('PUR008', 1, 1, 100.00),
+                                ('PUR008', 3, 3, 60.00),
+                                ('PUR008', 4, 1, 80.00),
+                                ('PUR009', 2, 1, 60.00),
+                                ('PUR009', 4, 2, 80.00),
+                                ('PUR010', 1, 2, 100.00),
+                                ('PUR010', 2, 1, 60.00),
+                                ('PUR010', 3, 2, 40.00),
+                                ('PUR011', 1, 2, 100.00),
+                                ('PUR011', 3, 1, 20.00),
+                                ('PUR011', 4, 2, 80.00),
+                                ('PUR012', 2, 1, 60.00),
+                                ('PUR012', 4, 2, 80.00),
+                                ('PUR013', 1, 1, 100.00),
+                                ('PUR013', 3, 3, 60.00),
+                                ('PUR014', 2, 1, 60.00),
+                                ('PUR014', 3, 1, 20.00),
+                                ('PUR014', 4, 2, 80.00);
+                        ";
 
                         using (var command = new MySqlCommand(insertQuery, connection, transaction))
                         {
                             int rowsAffected = command.ExecuteNonQuery();
                             if (rowsAffected <= 0)
-                            {
-                                throw new Exception("Error inserting data into the database");
-                            }
+                                throw new Exception("Error inserting sales data");
                         }
 
                         transaction.Commit();
                     }
-                    catch (Exception)
+                    catch (MySqlException)
                     {
                         transaction.Rollback();
                         throw;
@@ -321,8 +316,7 @@ namespace KEStoreApi.Data
                 }
             }
         }
-
-        public static async Task<IEnumerable<Product>> GetProductsFromDBaAsync()
+ public static async Task<IEnumerable<Product>> GetProductsFromDBaAsync()
         {
             List<Product> products = new List<Product>();
             string connectionString = DatabaseConfiguration.Instance.ConnectionString;
@@ -333,24 +327,21 @@ namespace KEStoreApi.Data
 
                 string query = "SELECT id, name, price, ImageUrl, idCategoria, description FROM products WHERE isDeleted = 0";
                 using (var command = new MySqlCommand(query, connection))
+                using (var reader = await command.ExecuteReaderAsync())
                 {
-                    var readerTask = command.ExecuteReaderAsync();
-                    using (var reader = await readerTask)
+                    while (await reader.ReadAsync())
                     {
-                        while (await reader.ReadAsync())
+                        int idCategoria = reader.GetInt32("idCategoria");
+                        products.Add(new Product
                         {
-                            int idCategoria = reader.GetInt32("idCategoria");
-                            products.Add(new Product
-                            {
-                                Id = reader.GetInt32("id"),
-                                Name = reader.GetString("name"),
-                                Price = reader.GetDecimal("price"),
-                                ImageUrl = reader.GetString("ImageUrl"),
-                                CategoriaId = idCategoria,
-                                Description = reader.GetString("description"),
-                                IsDeleted = false,
-                            });
-                        }
+                            Id = reader.GetInt32("id"),
+                            Name = reader.GetString("name"),
+                            Price = reader.GetDecimal("price"),
+                            ImageUrl = reader.GetString("ImageUrl"),
+                            CategoriaId = idCategoria,
+                            Description = reader.GetString("description"),
+                            IsDeleted = false,
+                        });
                     }
                 }
             }
