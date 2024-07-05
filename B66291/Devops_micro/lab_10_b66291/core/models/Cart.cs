d7b5d608a932cce51bda685225d49d886c66b56e
jazielrs <jazielrs1@gmail.com>
2024-07-05 17:34:41 -0600

    limpieza

--- B66291/Devops_micro/lab_10_b66291/core/models/Cart.cs
+++
@@ -1,8 +0,0 @@
-namespace core.Models;
-public sealed class Cart 
-{
-    public List <string> ProductIds { get; set; }
-    public string Address { get; set; }
-    public PaymentMethods.Type PaymentMethod { get; set; }
-
-}
